ee24bae3121f2694a8e7f9056160a4db7a83479f
Juan Tomás Araque Martinez <63049937+JTAraque@users.noreply.github.com>
2021-03-12 20:26:43 +0100

    First working version before deploy

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+const bodyParser = require('body-parser');
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 var app = express();
+
+require('dotenv').config();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +34,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
+
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit:'50mb',extended: true}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +56,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
+
 
 module.exports = app;
